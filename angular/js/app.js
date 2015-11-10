265fc4326a3f66db742b1ed95e4b2a45a1e8a503
pom421 <mauguet.po@gmail.com>
2015-11-10 15:13:32 +0100

    ajout angular-phonecat

--- angular/js/app.js
+++ angular/js/app.js
@@ -1,7 +1,5 @@
-var phonecatApp = angular.module('phonecatApp', [
-  'ngRoute',
-  'phonecatControllers'
-]);
+var phonecatApp = angular.module('phonecatApp', ['ngRoute','phonecatControllers','phonecatFilters']);
+
 
 phonecatApp.config(['$routeProvider',
   function($routeProvider) {
